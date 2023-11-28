3c1a36fc7584f6c35220dcb582e6d421afb3b216
shanb1712 <shanybiton@gmail.com>
2023-11-28 13:45:51 +0200

    add docstring to record_diagnosis

--- parsing/uvafdb_parser.py
+++ parsing/uvafdb_parser.py
@@ -164,6 +164,16 @@ class UVAFDB_Parser(BaseParser):
                 self.n_ectopics[pat][win] = np.load(self.main_path / pat / "n_ectopics" / (str(win) + ".npy"))
 
     def record_diagnosis(self, patient_id, win, AF_PERSISTENT_THRESHOLD=0.99):
+        """ This function creates a feature diagnosis. This diagnosis follows the convention of paroxysmal, persistant
+        or non-AF used in the clinical practice. The different categories of patients are: Non-AF (Time in AF
+        does not exceed 30 [sec], Persistent AF (AFB above AF_PERSISTENT_THRESHOLD), Paroxysmal AF (AFB between 4% and
+         AF_PERSISTENT_THRESHOLD and Time in AF exceed 30 [sec]). If the burden of a given pathology for a patient is
+         over 50%, we flage him as a patient suffering from another CVD (label cts.PATIENT_LABEL_OTHER_CVD). As a
+         convention, for windows, 0 is the label for NSR, 1 for AF, and above 2 for other rhythms.
+        :param patient_id: The patient ID. Assumed to be in the list of IDs present in the database.
+        :param win: The windows for which the feature should be computed.
+        :param AF_PERSISTENT_THRESHOLD: threshold for cutoff.
+        """
         raw_rr = self.rr_dict[patient_id][:(len(self.rr_dict[patient_id]) // win) * win].reshape(-1, win)[
             self.mask_rr_dict[patient_id][win]].reshape(-1)
         raw_rlab = self.rlab_dict[patient_id][:(len(self.rlab_dict[patient_id]) // win) * win].reshape(-1, win)[
