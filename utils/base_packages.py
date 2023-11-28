6ab1067e5eaf8470dc3d89b32dcc0848cb9737f3
shanb1712 <shanybiton@gmail.com>
2023-11-28 15:51:52 +0200

    move imports from jpaf to here

--- utils/base_packages.py
+++ utils/base_packages.py
@@ -42,7 +42,7 @@ import matplotlib.ticker as tick
 from scipy.signal import savgol_filter, butter, sosfreqz, sosfiltfilt
 import mne
 import scipy
-
+import datetime as dt
 sys.path.append('C:\\Users\\ShanyBiton\\Documents\\repos\\Generalization\\parsing\\')
 sys.path.append('C:\\Users\\ShanyBiton\\Documents\\repos\\Generalization\\utils\\')
 sys.path.append('/home/shanybiton/repos/Generalization')
