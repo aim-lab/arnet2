fce661f2e593cc0ac2d55d37bfce9941ca5d146e
shanb1712 <shanybiton@gmail.com>
2023-12-25 21:51:39 +0200

    refactor relative functions

--- parsing/spanish_parser.py
+++ parsing/spanish_parser.py
@@ -6,7 +6,7 @@ random.seed(cts.SEED)
 
 class SPANISH_Parser(BaseParser):
 
-    def __init__(self, window_size=60, visit=1, afib_lab_type='afib', load_on_start=True):
+    def __init__(self, window_size=60, visit=1, load_on_start=True):
         """Note: In SHHS and MESA, the window labels and the rlab dictionnary are not relevant. They are based on the global
         label of the patient (binary label for AF) which can be found among the variables. The afib_lab_type argument refers to
         the type of AF label used: AFIB, AFIB incident or AFIB prevalent. AFIB has been used all along.
@@ -28,37 +28,31 @@ class SPANISH_Parser(BaseParser):
         """Variables relative to the ECG signals."""
         self.orig_fs = 512  # Warning ! Some files present a different sample frequency !
         self.actual_fs = cts.EPLTD_FS
-        self.name = ""
-        self.ecg_format = "edf"
-        self.rhythms = np.array(['(N', '(AFIB'])
-        self.visit = visit
-
-        self.rhythms_dict = {self.rhythms[i]: i for i in range(len(self.rhythms))}
+        self.name = "SHHS"
+        self.ecg_format = ".edf"
 
         """Variables relative to the different paths"""
         self.raw_ecg_path = cts.DATA_DIR / 'copdosadb' / 'polysomnography' / 'edfs'
         self.orig_anns_path = None
         self.generated_anns_path = cts.PREPROCESSED_DATA_DIR / self.name
         self.annotation_types = np.intersect1d(np.array(os.listdir(self.generated_anns_path)), cts.ANNOTATION_TYPES)
-        self.filename = "SPANISH.pkl"
-        self.main_path = ""
-        self.window_size = window_size
+        self.main_path = cts.PREPROCESSED_DATA_DIR / self.name
 
         """ Checking the parsed window sizes and setting the window size. The data corresponding to the window size
         requested will be loaded into the system."""
 
-        if os.path.exists(self.main_path):
-            parsed_patients = self.parsed_patients()
-            test_pat = parsed_patients[0]
-            self.window_sizes = np.array([int(x[:-4]) for x in os.listdir(self.main_path / test_pat / "features")])
-            if load_on_start:
+        test_pat = self.parsed_patients()[0]
+        self.window_sizes = np.array([int(x[:-4]) for x in os.listdir(self.main_path / test_pat / "mask_rr")])
+        if load_on_start:
+            if os.path.exists(self.main_path):
                 self.set_window_size(self.window_size)
 
         """
         # ------------------------------------------------------------------------------- #
-        # ---------------- Local variables (relevant only for the SHHS) ----------------- #
+        # ---------------- Local variables (relevant only to SHHS) ----------------- #
         # ------------------------------------------------------------------------------- #
         """
+        self.visit = visit
 
     """
     # ------------------------------------------------------------------------- #
@@ -70,42 +64,42 @@ class SPANISH_Parser(BaseParser):
         # return np.array([file[6:12] for file in os.listdir(str(self.raw_ecg_path))])
         return np.array([file.split('.')[0] for file in os.listdir(str(self.raw_ecg_path))])
 
-    def parse_annotation(self, pat, type="epltd0"):
+    def parse_annotation(self, pat, lead, type="epltd0"):
         return wfdb.rdann(str(self.generated_anns_path / type / pat), type).sample
 
-    def record_to_wfdb(self, id):
-        file = self.raw_ecg_path / (id + ".edf")
+    def record_to_wfdb(self, id, lead):
+        file = self.raw_ecg_path / (id + self.ecg_format)
         edf = pyedflib.EdfReader(str(self.raw_ecg_path / file))
         ecg1_idx = np.where(np.array(edf.getSignalLabels()) == 'ECG1')[0][0]
         ecg_raw = edf.readSignal(ecg1_idx)
         fs = edf.getSampleFrequencies()[ecg1_idx]
         ecg_resampled = signal.resample(ecg_raw, int(len(ecg_raw) * self.actual_fs / fs))
         wfdb.wrsamp(str(id), fs=self.actual_fs, units=['mV'],
-                    sig_name=['V5'], p_signal=ecg_resampled.reshape(-1, 1), fmt=['16'], write_dir='/home/shanybiton/repos/Generalization/parsing/')
+                    sig_name=['V5'], p_signal=ecg_resampled.reshape(-1, 1), fmt=['16'], )
         self.curr_edf = edf
         return ecg_resampled
 
-    def parse_raw_ecg(self, patient_id, start=0, end=-1, type='epltd0'):
-        edf = pyedflib.EdfReader(str(self.raw_ecg_path / ('shhs' + str(self.visit) + '-' + patient_id + '.edf')))
+    def parse_raw_ecg(self, patient_id, lead, start=0, end=-1, type='epltd0'):
+        edf = pyedflib.EdfReader(str(self.raw_ecg_path / ('shhs' + str(self.visit) + '-' + patient_id + self.ecg_format)))
         self.curr_edf = edf
-        ecg_idx = np.where(np.array(edf.getSignalLabels()) == 'ECG')[0][0]
+        ecg_idx = np.where(np.array(edf.getSignalLabels()) == 'ECG')[0][lead-1]
         ecg_raw = edf.readSignal(ecg_idx)
         Fs = np.round(edf.samplefrequency(ecg_idx))
         ecg = signal.resample(ecg_raw, int(len(ecg_raw) * cts.EPLTD_FS / Fs))
-        ann = self.parse_annotation(patient_id, type=type)
+        edf.close()
         if end == -1:
             end = int(len(ecg) / self.actual_fs)
         start_sample = start * self.actual_fs
         end_sample = end * self.actual_fs
         ecg = ecg[start_sample:end_sample]
+        ann = self.parse_annotation(patient_id, type=type)
         ann = ann[np.where(np.logical_and(ann >= start_sample, ann < end_sample))]
         ann -= start_sample
-        edf.close()
         return ecg, ann
 
-    # rlab is not relevant here. Same for af burden. Will have rlab all ones and zeros, and af burden 0 or 100%. They should not be considered.
+    # rlab is not relevant here. Same for af burden. Will have rlab all ones and zeros, and af burden 0 or 100%. They
+    # should not be considered.
     def parse_elem_data(self, pat):
-
         dicts_to_fill = [self.start_windows_dict, self.end_windows_dict, self.av_prec_windows_dict, self.n_excluded_windows_dict, self.mask_rr_dict]
         for dic in dicts_to_fill:
             if pat not in dic.keys():
@@ -157,7 +151,30 @@ class SPANISH_Parser(BaseParser):
         det_desat, table_desat_aa, table_desat_bb, table_desat_cc = fc.sc_desaturations(data_resamp_med)
         total_recording_time = ((n_spo2 / sf_spo2) / cts.N_SEC_IN_MIN) / cts.N_MIN_IN_HOUR  # In hours
         self.odi_dict[id] = len(table_desat_aa) / total_recording_time  # Definition of ODI: Number of desaturations per hour
-    # ------------------------------------------------------------------------------------ #
+
+    def parse_demographic_features(self, id):
+        # Age (age_s1), Hypertension (HTNDerv_s + visit), metabolic syndrome (??), BMI (bmi_s+visit), gender (gender,
+        # 1 male, 2 female), necksize (neck20, only in shhs1)
+        age = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["age_s" + str(self.visit)])
+        hypertension = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["HTNDerv_s" + str(self.visit)])
+        bmi = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["bmi_s" + str(self.visit)])
+        gender = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["gender"])
+        if self.visit == 1:
+            necksize = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["NECK20"])
+        else:
+            path = cts.DATA_DIR / 'shhs' / 'datasets' / 'shhs1-dataset-0.14.0.csv'
+            tab = pd.read_csv(path)
+            necksize = float(tab[tab["nsrrid"] == int(id)]["NECK20"])
+        dict_dem = {"Age": age, "Hypertension": hypertension, "BMI": bmi, "Gender": gender, "necksize": necksize}
+        for win in self.window_sizes:
+            for dem_name, dem_val in dict_dem.items():
+                self.features_dict[id][win][dem_name] = dem_val
+
+    """
+    # ------------------------------------------------------------------------- #
+    # ---------------- Functions relative to this dataset only ---------------- #
+    # ------------------------------------------------------------------------- #
+    """
 
     def reannotate_af_label(self):
         """ This function sets new labels to the recordings re-annotated by the peer cardiologist."""
@@ -184,35 +201,17 @@ class SPANISH_Parser(BaseParser):
                               x_titles=[['Rhythm types']], y_titles=[["Count"]])
         plt.show()
 
-    def parse_demographic_features(self, id):
-        # Age (age_s1), Hypertension (HTNDerv_s + visit), metabolic syndrome (??), BMI (bmi_s+visit), gender (gender, 1 male, 2 female), necksize (neck20, only in shhs1)
-        age = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["age_s" + str(self.visit)])
-        hypertension = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["HTNDerv_s" + str(self.visit)])
-        bmi = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["bmi_s" + str(self.visit)])
-        gender = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["gender"])
-        if self.visit == 1:
-            necksize = float(self.afib_tab[self.afib_tab["nsrrid"] == int(id)]["NECK20"])
-        else:
-            path = cts.DATA_DIR / 'shhs' / 'datasets' / 'shhs1-dataset-0.14.0.csv'
-            tab = pd.read_csv(path)
-            necksize = float(tab[tab["nsrrid"] == int(id)]["NECK20"])
-        dict_dem = {"Age": age, "Hypertension": hypertension, "BMI": bmi, "Gender": gender, "necksize": necksize}
-        for win in self.window_sizes:
-            for dem_name, dem_val in dict_dem.items():
-                self.features_dict[id][win][dem_name] = dem_val
-
-
 if __name__ == '__main__':
     db = SPANISH_Parser(load_on_start=False)
-    pat_list = db.parse_available_ids()
-    pat_list = pat_list[~np.isin(pat_list, db.missing_ecg)]
+    # pat_list = db.parse_available_ids()
+    # pat_list = pat_list[~np.isin(pat_list, db.missing_ecg)]
     # for id_ in pat_list:
     #     file = db.raw_ecg_path / (id_ + ".edf")
     #     edf = pyedflib.EdfReader(str(file))
     #     if len(np.where(np.array(edf.getSignalLabels()) == 'ECG1')[0]) == 0:
     #         db.missing_ecg = np.append(db.missing_ecg, id_)
 
-    db.generate_annotations(types=('xqrs'), pat_list=pat_list, force=False)
+    # db.generate_annotations(types=('xqrs'), pat_list=pat_list, force=False)
 
     #
     # for pat in to_load:
