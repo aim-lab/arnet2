d90fbbeb19f0e8abfa9d371e6ee981b2ff24d659
shanb1712 <shanybiton@gmail.com>
2023-12-25 20:56:23 +0200

    refactor parse_annotation

--- parsing/mesa_parser.py
+++ parsing/mesa_parser.py
@@ -66,7 +66,9 @@ class MESA_Parser(BaseParser):
     def parse_available_ids(self):
         return np.array([file[11:15] for file in os.listdir(str(self.raw_ecg_path))])
 
-    def parse_annotation(self, id, type="epltd0", lead=1):
+    def parse_annotation(self, id, lead, type="epltd0"):
+        if type not in self.annotation_types:
+            raise IOError("The requested annotation does not exist.")
         return wfdb.rdann(str(self.generated_anns_path / type / str(lead) / id), type).sample
 
     def record_to_wfdb(self, id, lead=1):
