9dd3f2cb4540fad45f3b14c33fa0ba5a7c29fdf8
shanb1712 <shanybiton@gmail.com>
2023-12-26 14:01:25 +0200

    update raw_ecg_path

--- parsing/uvafdb_parser.py
+++ parsing/uvafdb_parser.py
@@ -31,7 +31,7 @@ class UVAFDB_Parser(BaseParser):
         self.ecg_format = ".rf"
 
         """Variables relative to the different paths"""
-        self.raw_ecg_path = cts.DATA_DIR
+        self.raw_ecg_path = cts.DATA_DIR / 'uvfdb'
         self.orig_anns_path = None
         self.generated_anns_path = cts.GEN_ANN_DIR / self.name
         self.annotation_types = np.intersect1d(np.array(os.listdir(self.generated_anns_path)), cts.ANNOTATION_TYPES)
