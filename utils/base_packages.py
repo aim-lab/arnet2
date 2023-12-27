39732a620d821a91812db75fa53e04aed6445a7c
shanb1712 <shanybiton@gmail.com>
2023-12-27 10:39:37 +0200

    rearrange base_packages.py

--- utils/base_packages.py
+++ utils/base_packages.py
@@ -7,49 +7,56 @@ try:
     import utils.consts as cts
 except ModuleNotFoundError:
     import consts as cts
+
+# management operations
 import os
 import shutil
 import sys
 import warnings
+import re
+import glob
+import pathlib
 
 # Data Processing
 import numpy as np
+import wfdb
 import wfdb.processing as processing
 import pandas as pd
-import scipy.signal as signal
 import multiprocessing
 import subprocess
 import itertools
 import math
+import random
+import scipy
+import scipy.signal as signal
+from scipy.signal import savgol_filter, butter, sosfreqz, sosfiltfilt
+import mne
 
 # Graphics
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
 
 # I/O
 import csv
 import xlrd
 import xlsxwriter
 import scipy.io as sio
-import wfdb
 import pyedflib
 import joblib
 import zipfile
 import io
 import copy
-import random
 import pickle
-import re
-import matplotlib.ticker as tick
-from scipy.signal import savgol_filter, butter, sosfreqz, sosfiltfilt
-import mne
-import scipy
+import mat73
+
+# time-related functions
 import datetime as dt
+import time
+
+#  machine learning
 from sklearn.linear_model import LinearRegression
 from scipy.spatial import cKDTree
-import mat73
-import time
-import glob
-import pathlib
+
 np.random.seed(cts.SEED)
 random.seed(cts.SEED)
 warnings.filterwarnings('ignore')
