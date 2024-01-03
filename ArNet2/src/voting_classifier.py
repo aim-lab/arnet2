072e735c0bdcce5abf2954d94a587b03a796fb39
shanb1712 <shanybiton@gmail.com>
2024-01-03 09:32:15 +0200

    add documentation

--- ArNet2/src/voting_classifier.py
+++ ArNet2/src/voting_classifier.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.preprocessing import LabelEncoder
 import sys
 
-#relative paths
+# relative paths
 from src.models import metrics
 
 sys.path.append('/home/shanybiton/repos/Generalization')
@@ -24,10 +24,18 @@ import data.data_loading as data_loading
 from parsing.db_loader import *
 import src.models.metrics as metrics
 
+
 class CostumedVotingClassifier(VotingClassifier):
+    """
+    Class defining a voting classifier.
+    This class offers the possibility to load the classifiers onto the system.
+    It further predicts probabilities based on the classifiers' threshold.
+    """
 
     def _predict(self, X):
-        """Collect results from clf.predict calls."""
+        """
+        Collect results from clf.predict_proba calls and further predicts labels based on a decision_th.
+        """
         stackX = None
         for est in self.estimators_:
             decision_th = est['best_th']
@@ -40,7 +48,9 @@ class CostumedVotingClassifier(VotingClassifier):
         return stackX[0]
 
     def _collect_probas(self, X):
-        """Collect results from clf.predict calls."""
+        """
+        Collect results from clf.predict_proba calls.
+        """
         stackX = None
         for est in self.estimators_:
             probas = est['classifier'].predict_proba(X)[:, 1]
@@ -50,6 +60,7 @@ class CostumedVotingClassifier(VotingClassifier):
                 stackX = np.dstack((stackX, probas))
         return stackX[0]
 
+
 if __name__ == '__main__':
     # Load data
     print("Loading Data...")
@@ -57,13 +68,14 @@ if __name__ == '__main__':
 
     print("Loading voting classifier...")
     members = data_loading.load_all_models(models=['ArNet', 'ArNet2', 'ArNet2+DA'])
-    eclf = CostumedVotingClassifier(estimators=[(key, model['classifier']) for key, model in members.items()], weights=[1, 2, 1.5])
+    eclf = CostumedVotingClassifier(estimators=[(key, model['classifier']) for key, model in members.items()],
+                                    weights=[1, 2, 1.5])
     _, y, _ = test_dict['test_JPAFDB']
     eclf.estimators_ = list(members.values())
     eclf.le_ = LabelEncoder().fit(y)
     eclf.classes_ = eclf.le_.classes_
 
-    error_analysis =True
+    error_analysis = True
     metrics_dict, best_th_dict, mean_abs_afb_error_dict = {}, {}, {}
     for set_name, set_data in test_dict.items():
         print(f"evaluating performance on {set_name}...")
@@ -72,14 +84,16 @@ if __name__ == '__main__':
         yhat = eclf.predict(X)
         probas = eclf._collect_probas(X)
         metrics_dict[set_name] = metrics.model_metrics(np.zeros(shape=np.shape(y)), y, yhat, print_metrics=True)
-        mean_abs_afb_error_dict[set_name] = metrics.mean_abs_afb_error(X, y, yhat)  # takes a lot of time, so maybe we should comment it for now before making it faster
+        mean_abs_afb_error_dict[set_name] = metrics.mean_abs_afb_error(X, y,
+                                                                       yhat)  # takes a lot of time, so maybe we should comment it for now before making it faster
 
         if error_analysis:
             df = pd.DataFrame([])
             df['id'] = X[:, -1]
             df = df.join(pd.DataFrame.from_dict(dict(zip(list(members.keys()), probas.T)), orient='columns'))
             df = df.join(pd.DataFrame.from_dict(dict(zip([member + ' decision threshold' for member in members.keys()],
-                                                    np.tile([model['best_th'] for model in members.values()], np.shape(y.reshape(-1, 1))).T)), orient='columns'))
+                                                         np.tile([model['best_th'] for model in members.values()],
+                                                                 np.shape(y.reshape(-1, 1))).T)), orient='columns'))
             df['yhat'] = yhat
             df['lab'] = y
             df['start_time'] = t_s[:, 0]
@@ -87,5 +101,3 @@ if __name__ == '__main__':
             df['set_name'] = set_name
             model_utils.save_df(df,
                                 f"/home/shanybiton/repos/Generalization/output/VotingClassifier_{set_name}_pred.csv")
-
-
