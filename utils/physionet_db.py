4c71ebfa8a6522aff64db205bb416408190727fc
shanb1712 <shanybiton@gmail.com>
2024-07-29 21:41:20 +0300

    add interval description

--- utils/physionet_db.py
+++ utils/physionet_db.py
@@ -39,7 +39,7 @@ def prepare_deatil_pat(df):
 #     return rhythm_str, loc
 
 
-def replace_duplicates_with_placeholder(rhythm, rhythm_dict, placeholder='"'):
+def replace_duplicates_with_placeholder(rhythm, rhythm_dict, placeholder=''):
     rhythm_str = np.array([rhythm_dict[i] for i in rhythm])
     seen = ['']
     result = []
@@ -74,8 +74,9 @@ if __name__ == '__main__':
         #     [db.parse_raw_ecg(pat, type=ann_type, lead=lead)[0].reshape(-1, 1) for lead in
         #      range(1, db.n_leads + 1)]), axis=1)
         ann = db.parse_raw_ecg(pat, lead=db.ref_lead, type=ann_type)[1]
-        rhythm_ = db.parse_medaim_annotations(pat, np.cumsum(ann) / cts.N_MS_IN_S)
-        rhythm = replace_duplicates_with_placeholder(rhythm_, rhythm_dict)
+        # rrt_dict = np.concatenate(([ann[0] / db.actual_fs], np.cumsum(ann) + ann[0] / db.actual_fs))
+        rhythm_ = db.parse_medaim_annotations(pat, ann/db.actual_fs)
+        rhythm = replace_duplicates_with_placeholder(rhythm_, rhythm_dict, placeholder='')
         print(f'rhythms available: {np.unique(rhythm)}')
         res = list(zip(*np.unique(rhythm_, return_counts=True)))
         for re in res:
@@ -86,11 +87,16 @@ if __name__ == '__main__':
         #             base_time=datetime.time(*return_time_from_millis(db.circadian_dict[pat]['start_recording'])),
         #             base_date=datetime.date(db.circadian_dict[pat]['recording_date'].year, 1, 1),
         #             write_dir=str(dest_path))  # ecgs
-        wfdb.wrann(pat, 'atr', sample=ann, aux_note=rhythm, fs=db.actual_fs,
+        wfdb.wrann(pat, 'atr', sample=ann, aux_note=list(rhythm), fs=db.actual_fs,
                    write_dir=str(dest_path), label_store=np.array([22]*len(rhythm_)))  # , label_store=np.array([1] * len(ann)))  # annotations
-        # wfdb.wrann(pat, 'qrs', ann, aux_note=np.array([' '] * len(ann)), fs=db.actual_fs,
-        #            write_dir=str(dest_path),
-        #            symbol=np.array(['N'] * len(ann)))  # , label_store=np.array([1] * len(ann)))  # annotations
+        wfdb.wrann(pat, 'qrs', ann, aux_note=np.array([' '] * len(ann)), fs=db.actual_fs,
+                   write_dir=str(dest_path),
+                   symbol=np.array(['N'] * len(ann)))  # , label_store=np.array([1] * len(ann)))  # annotations
+
+    rhythm_df = db.parse_medaim_reference_rhythm(pat_list[0])
+    for pat in pat_list[1:]:
+        rhythm_df = rhythm_df.append(db.parse_medaim_reference_rhythm(pat))
+    print(rhythm_df.groupby('Class').count())
     # detail_pat = db.excel_sheet.loc[db.excel_sheet['Study ID'].isin(pat_list)]
     # detail_pat = prepare_deatil_pat(detail_pat)
     # detail_pat.to_csv(dest_path / 'AdditionalData.csv', index=False)
